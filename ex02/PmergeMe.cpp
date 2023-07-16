dfd0ee400a419601685ed17c1fc384e668f23791
Rath Thammatucharee <rthammat@z3t9c1.42bangkok.com>
2023-07-16 20:34:52 +0700

    ex02 constructor pending

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: rthammat <rthammat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 18:39:59 by rthammat          #+#    #+#             */
-/*   Updated: 2023/07/16 18:55:30 by rthammat         ###   ########.fr       */
+/*   Updated: 2023/07/16 20:34:10 by rthammat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,20 @@ PmergeMe::PmergeMe(const PmergeMe &src)
 	if (this != &src)
 	{
 		this->_input = src._input;
-		if ()
-		this->_vec = src._vec;
-		this->_lst = src._lst;
+		if (!this->_vec.empty())
+		{
+			this->_vec.clear();
+			for (size_t i = 0; i < this->_vec.size(); ++i)
+				this->_vec.push_back(src._vec[i]);
+		}
+		if (!this->_lst.empty())
+		{
+			this->_lst.clear();
+			std::list<int> tmp = src._lst;
+			for (std::list<int>::iterator it = tmp.begin(); it != tmp.end(); ++it)
+				this->_lst.push_back(*it);
+			this->_lst = src._lst;
+		}
 	}
 }
 
